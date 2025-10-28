7da9a8651b4abc9235d7476e20a09fc5a4986530
Park-Ocean <bastianapablazavega@gmail.com>
2025-10-28 20:32:01 -0300

    feat(pages): configurar react-router y agregar placeholders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
+// Importaciones de los holder de pages
+import AgendarIntegral from './pages/AgendarIntegral' 
+import AgendarMetodologo from './pages/AgendarMetodologo'
+import AgendarPsicologo from './pages/AgendarPsicologo'
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* navBar del header podria ir aqui */}
+      <Routes> {/* observa y compara la url actual con las almacenadas*/}
+        <Route path="/" element={<Home/>} /> {/* indica que debe renderizar [element] si coinciden */}
+        <Route path="/AgendarIntegral" element={<AgendarIntegral />} />
+        <Route path="/AgendarMetodologo" element={<AgendarMetodologo />} />
+        <Route path="/AgendarPsicologo" element={<AgendarPsicologo />} />
+      </Routes>
+      {/* Footer podria ir aqui */}
     </>
-  )
+  );
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"; // Importa bootstrap  nivel global
+import {BrowserRouter} from 'react-router-dom' // Importaciones de react-router
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  //Strictmode: Ejecuta el codigo 2 veces. Verifica q el codigo este "limpio"
+  <StrictMode> 
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
