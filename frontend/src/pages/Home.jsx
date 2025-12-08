bcdaabaf589d7d08055c7d272a52fcbc2a000c1d
ignacio0822 <xicono8939@gmail.com>
2025-12-08 17:13:02 -0300

    feat(frontend): agregar anclas de navegación (Element) para servicios y footer en Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,22 +7,29 @@ import AboutSection from "../components/AboutSection/AboutSection.jsx";
 import VideoSection from "../components/VideoSection/VideoSection.jsx";
 import FloatingWhatsApp from "../components/FloatingWhatsApp/FloatingWhatsApp.jsx";
 
-
 function Home() {
   return (
     <div>
       <Hero />
 
+      {/* El botón "Ver Planes" buscará este nombre */}
       <Element name="seccion-servicios">
         <ServicesSection />
       </Element>
+
       <Element name="seccion-nosotros">
         <AboutSection />
       </Element>
+
       <VideoSection />
+
       <Element name="seccion-FAQ">
         <FAQSection />
       </Element>
+
+      <Element name="seccion-contacto">
+      </Element>
+
       <FloatingWhatsApp />
     </div>
   );
